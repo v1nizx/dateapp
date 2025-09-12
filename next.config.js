2518a69e4b069a7f26f71c17d600b3e1520907e5
marcos.rios <marcos.rios@tcema.tc.br>
2025-09-12 08:47:31 -0300

    s

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  experimental: {
+    appDir: false,
+  },
+}
+
+module.exports = nextConfig
