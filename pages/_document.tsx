31b375efe475cce513e67f522a34de36d8bdb908
marcos.rios <marcos.rios@tcema.tc.br>
2025-10-17 12:26:28 -0300

    anuncios

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9683441595888028"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
