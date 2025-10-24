0c3a93dce870ba60810e8efc5ef34fd19c955916
marcos.rios <marcos.rios@tcema.tc.br>
2025-10-24 09:54:51 -0300

    up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,29 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9683441595888028"
           crossOrigin="anonymous"
         ></script>
+        <script
+          async
+          custom-element="amp-ad"
+          src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
+        ></script>
       </Head>
       <body>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `
+              <amp-ad
+                width="100vw"
+                height="320"
+                type="adsense"
+                data-ad-client="ca-pub-9683441595888028"
+                data-ad-slot="7995557049"
+                data-auto-format="rspv"
+                data-full-width="">
+                <div overflow=""></div>
+              </amp-ad>
+            `
+          }}
+        />
         <Main />
         <NextScript />
       </body>
